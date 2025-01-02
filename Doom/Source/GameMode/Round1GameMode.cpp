f83b3910bc7b7687dd0deb2506428ba12f276962
yongilkim01 <yongil.k01@gmail.com>
2025-01-02 10:37:19 +0900

    chore: update camera move speed

--- Doom/Source/GameMode/Round1GameMode.cpp
+++ Doom/Source/GameMode/Round1GameMode.cpp
@@ -44,18 +44,18 @@ void ARound1GameMode::Tick(float DeltaTime)
 
 	if (UEngineInput::IsPress('A'))
 	{
-		Camera->AddActorLocation(FVector{ -200.0f * DeltaTime, 0.0f, 0.0f });
+		Camera->AddActorLocation(FVector{ -2000.0f * DeltaTime, 0.0f, 0.0f });
 	}
 	if (UEngineInput::IsPress('D'))
 	{
-		Camera->AddActorLocation(FVector{ 200.0f * DeltaTime, 0.0f, 0.0f });
+		Camera->AddActorLocation(FVector{ 2000.0f * DeltaTime, 0.0f, 0.0f });
 	}
 	if (UEngineInput::IsPress('W'))
 	{
-		Camera->AddActorLocation(FVector{ 0.0f, 0.0f, 200.0f * DeltaTime, 0.0f });
+		Camera->AddActorLocation(FVector{ 0.0f, 0.0f, 2000.0f * DeltaTime, 0.0f });
 	}
 	if (UEngineInput::IsPress('S'))
 	{
-		Camera->AddActorLocation(FVector{ 0.0f, 0.0f, -200.0f * DeltaTime, 0.0f });
+		Camera->AddActorLocation(FVector{ 0.0f, 0.0f, -2000.0f * DeltaTime, 0.0f });
 	}
 }
